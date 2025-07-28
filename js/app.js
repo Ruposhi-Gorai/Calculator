735499ca40feedc6db35f10bacf0f3b596f650e1
Ruposhi <ruposhigorai27@gmail.com>
2025-07-28 12:39:46 +0530

    bug fixed

--- js/app.js
+++ js/app.js
@@ -1,35 +1,43 @@
-let input = document.getElementById('num1');
-let ans = document.getElementById('num2');
-let buttons = document.querySelectorAll('button');
+let input = document.getElementById("num1");
+let ans = document.getElementById("num2");
+let buttons = document.querySelectorAll("button");
 let exp = "";
+let opr = ["+", "-", "*", "/", "."];
 let brackettoggle = true;
 
 buttons.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        let value = btn.value;
-        if (btn.id === "bracket") {
-          value = brackettoggle ? "(" : ")";
-          brackettoggle = !brackettoggle; // toggle
-        }
-        if (value === 'C') {
-            exp = "";
-            input.innerText = "";
-            ans.innerText = "";
-            brackettoggle = true;
-        } else if (value === '=') {
-            try {
-                let result = math.evaluate(exp);
-                ans.innerText = result;
-                exp = result.toString();
-            }
-            catch {
-                ans.innerText = 'error';
-                exp = "";
-            }
-            
-        } else {
-            exp += value;
-            input.innerText = exp;
-        }
-    });
+  btn.addEventListener("click", (e) => {
+    let value = e.target.value;
+
+    if (btn.id === "bracket") {
+      value = brackettoggle ? "(" : ")";
+      brackettoggle = !brackettoggle; // toggle
+    }
+
+    if (value === "C") {
+      exp = "";
+      input.innerText = "";
+      ans.innerText = "";
+      brackettoggle = true;
+    } else if (value === "=") {
+      let result = math.evaluate(exp);
+      ans.innerText = result;
+      exp = result.toString();
+    } else {   
+       if (exp === "" && opr.includes(value)) {
+         ans.innerText = "Invalid input"; // show error
+         return; // stop further processing
+       } else {
+         ans.innerText = "";
+       }
+      let lastChar = exp[exp.length - 1];
+      if (opr.includes(lastChar) && opr.includes(value)) {
+        exp = exp.slice(0, exp.length - 1) + value;
+      } else {
+        exp += value;
+      }
+      input.innerText = exp;
+      }
+     
+  });
 });
